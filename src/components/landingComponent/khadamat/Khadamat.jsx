52a511b0e2a94cb018bb898e4e416fff79a8f754
amir mohamad <a313gputimal@gmail.com>
2024-10-20 22:54:53 +0330

    res1

--- src/components/landingComponent/khadamat/Khadamat.jsx
+++ src/components/landingComponent/khadamat/Khadamat.jsx
@@ -12,6 +12,153 @@ const Khadamat = () => {
         <>
         <div>
 
+            <div className='hidden md:block lg:hidden mb-36'>
+                <div className='mx-auto flex justify-center flex-wrap w-[90%] mb-4'>
+                    <img className='w-5 h-5 mt-3 ml-2' src="../../../../public/imgLand/Ellipse 5.png"/>
+                    <h1 className='text-3xl font-extrabold'>
+                        خدمات ما
+                    </h1>
+                </div>
+                <p className='text-lg font-bold text-slate-500 text-center mb-16'>
+                    مجموعه ای از تمام آنچه شما نیاز دارید
+                </p>
+                <div className='flex justify-center gap-5 flex-wrap mx-auto'>
+
+                    <div className="w-1/5 h-[180px] relative mt-20">
+                        <img className='mx-auto w-1/3' src={Group6} />
+                        <img className='absolute bottom-0 w-full h-[150px]' src={Rec15} />
+                        <div className='relative w-1/2 mx-auto mt-3 mb-[50px]'>
+                            <h2 className='z-10 absolute text-2xl inset-x-2 font-bold'>
+                                مشاوره  
+                            </h2>
+                        </div>
+                        <div className='w-11/12 relative mx-auto '>
+                            <p className='z-10 absolute text-sm font-medium text-slate-500 text-center'>
+                                متن ساختگی صنعت طراحی و چاپ برای استفاده طراحان گرافیک است  
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="w-1/5 h-[180px] relative">
+                        <img className='mx-auto w-1/3' src={Group5} />
+                        <img className='absolute bottom-0 w-full h-[150px]' src={Rec14} />
+                        <div className='relative w-1/2 mx-auto mt-3 mb-[50px]'>
+                            <h2 className='z-10 absolute text-2xl inset-x-2 font-bold'>
+                                مشاوره  
+                            </h2>
+                        </div>
+                        <div className='w-11/12 relative mx-auto '>
+                            <p className='z-10 absolute text-sm font-medium text-slate-500 text-center'>
+                                متن ساختگی صنعت طراحی و چاپ برای استفاده طراحان گرافیک است  
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="w-1/5 h-[180px] relative mt-20">
+                        <img className='mx-auto w-1/3' src={Group4} />
+                        <img className='absolute bottom-0 w-full h-[150px]' src={Rec13} />
+                        <div className='relative w-1/2 mx-auto mt-3 mb-[50px]'>
+                            <h2 className='z-10 absolute text-2xl inset-x-2 font-bold'>
+                                مشاوره  
+                            </h2>
+                        </div>
+                        <div className='w-11/12 relative mx-auto '>
+                            <p className='z-10 absolute text-sm font-medium text-slate-500 text-center'>
+                                متن ساختگی صنعت طراحی و چاپ برای استفاده طراحان گرافیک است  
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="w-1/5 h-[180px] relative">
+                        <img className='mx-auto w-1/3' src={Group3} />
+                        <img className='absolute bottom-0 w-full h-[150px]' src={Rec12} />
+                        <div className='relative w-1/2 mx-auto mt-3 mb-[50px]'>
+                            <h2 className='z-10 absolute text-2xl inset-x-2 font-bold'>
+                                مشاوره  
+                            </h2>
+                        </div>
+                        <div className='w-11/12 relative mx-auto '>
+                            <p className='z-10 absolute text-sm font-medium text-slate-500 text-center'>
+                                متن ساختگی صنعت طراحی و چاپ برای استفاده طراحان گرافیک است  
+                            </p>
+                        </div>
+                    </div>     
+                </div>
+            </div>            
+
+            <div className='hidden lg:block xl:hidden mb-36'>
+                <div className='mx-auto flex justify-center mb-4'>
+                    <img className='w-5 h-5 mt-3 ml-2' src="../../../../public/imgLand/Ellipse 5.png"/>
+                    <h1 className='text-4xl font-extrabold'>
+                        خدمات ما
+                    </h1>
+                </div>
+                <p className='text-xl font-bold text-slate-500 text-center mb-10'>
+                    مجموعه ای از تمام آنچه شما نیاز دارید
+                </p>
+                <div className='flex justify-center gap-10'>
+                    <div className="w-1/5 h-[260px] relative mt-[100px]">
+                        <img className='mx-auto mt-[5%]' src={Group6} />
+                        <img className='absolute bottom-[10%] w-full h-[65%]' src={Rec15} />
+                        <div className='relative w-24 mx-auto mt-3 mb-[65px]'>
+                            <h2 className='z-10 absolute text-3xl font-extrabold'>
+                                مشاوره  
+                            </h2>
+                        </div>
+                        <div className='w-3/4 relative mx-auto'>
+                            <p className='z-10 absolute text-sm font-bold text-slate-500 text-center'>
+                                متن ساختگی صنعت طراحی و چاپ برای استفاده طراحان گرافیک است  
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="w-1/5 h-[260px] relative mb-[100px]">
+                        <img className='mx-auto mt-[5%]' src={Group5} />
+                        <img className='absolute bottom-[10%] w-full h-[65%]' src={Rec14} />
+                        <div className='relative w-24 mx-auto mt-3 mb-[65px]'>
+                            <h2 className='z-10 absolute text-3xl font-extrabold'>
+                                مشاوره  
+                            </h2>
+                        </div>
+                        <div className='w-3/4 relative mx-auto'>
+                            <p className='z-10 absolute text-sm font-bold text-slate-500 text-center'>
+                                متن ساختگی صنعت طراحی و چاپ برای استفاده طراحان گرافیک است  
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="w-1/5 h-[260px] relative mt-[100px]">
+                        <img className='mx-auto mt-[5%]' src={Group4} />
+                        <img className='absolute bottom-[10%] w-full h-[65%]' src={Rec13} />
+                        <div className='relative w-24 mx-auto mt-3 mb-[65px]'>
+                            <h2 className='z-10 absolute text-3xl font-extrabold'>
+                                مشاوره  
+                            </h2>
+                        </div>
+                        <div className='w-3/4 relative mx-auto'>
+                            <p className='z-10 absolute text-sm font-bold text-slate-500 text-center'>
+                                متن ساختگی صنعت طراحی و چاپ برای استفاده طراحان گرافیک است  
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="w-1/5 h-[260px] relative mb-[100px]">
+                        <img className='mx-auto mt-[5%]' src={Group3} />
+                        <img className='absolute bottom-[10%] w-full h-[65%]' src={Rec12} />
+                        <div className='relative w-24 mx-auto mt-3 mb-[65px]'>
+                            <h2 className='z-10 absolute text-3xl font-extrabold'>
+                                مشاوره  
+                            </h2>
+                        </div>
+                        <div className='w-3/4 relative mx-auto'>
+                            <p className='z-10 absolute text-sm font-bold text-slate-500 text-center'>
+                                متن ساختگی صنعت طراحی و چاپ برای استفاده طراحان گرافیک است  
+                            </p>
+                        </div>
+                    </div>                    
+                </div>
+            </div>
+
             <div className="hidden xl:block mb-36">
                 <div className='mx-auto flex justify-center mb-4'>
                     <img className='w-5 h-5 mt-3 ml-2' src="../../../../public/imgLand/Ellipse 5.png"/>
