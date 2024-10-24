ca2a2d9c44f43ec7789b9fccc7e58cea9c4e557d
MohmmdHosein-Gouran <mohammadhoseingrn@gmail.com>
2024-10-24 17:48:55 +0330

    landing3

---
+++ src/components/landingComponent/title/picture.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import "./picture.css"
+
+const picture = () => {
+    return(
+        <div className="box2">
+            <img src="images/hero section pic.png"></img>
+            <img className="halghe" src="images/Ellipse 4.png"></img>
+
+            <div className="circle2">
+                <img src="images/Circled Play.png"></img>
+                
+            </div>
+            <img className="kolah" src="images/Flying Mortarboard.png"></img>
+
+            <div className="student">+1000 دانشجو  
+            <div className="circle3">
+                <img src="images/Student Male.png"></img>
+            </div>
+            </div>
+
+            <div className="teacher">+50 مدرس
+                <div className="circle4">
+                    <img src="images/Teacher.png"></img>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default picture;
