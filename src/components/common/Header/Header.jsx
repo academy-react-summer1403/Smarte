51528a9998219f9f031fb79980707f4b9806a776
esmail <you@example.com>
2024-10-24 13:07:33 +0330

    icon header

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -4,8 +4,8 @@ import React, { useState } from 'react'
 import './Header.css'
 import Logo from '../../../assets/images/logo.png'
 import ShoppingBagIcon from '../../../assets/images/Shopping Bag.png'
-
-
+import Favorite from '../../../assets/images/Favorite.png'
+import User from '../../../assets/images/User.png'
 const Header = () => {
   const [activeTitle, setActiveTitle] = useState('صفحه نخست')
 
@@ -38,34 +38,16 @@ const Header = () => {
 
       <div className='header-left'>
         <button className='nav-button heart-button'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 24 24'
-            fill='none'
-            stroke='gray'
-            strokeWidth='2'
-            className='heart-icon'
-          >
-            <path d='M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z' />
-          </svg>
+          <img src={Favorite} alt='Shopping Bag'  className='account-icon'/>
         </button>
         <button className='nav-button cart-button'>
-          <img src={ShoppingBagIcon} alt='Shopping Bag' />
+          <img src={ShoppingBagIcon} alt='Shopping Bag'  className='account-icon' />
         </button>
 
         <button className='nav-button account-button'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 24 24'
-            fill='none'
-            stroke='white'
-            strokeWidth='2'
-            className='account-icon'
-          >
-            <path d='M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4z' />
-            <path d='M12 14c-4 0-8 2-8 6v1h16v-1c0-4-4-6-8-6z' />
-          </svg>
-          حساب کاربری
+        حساب کاربری
+            <img src={User} alt='User'  className='account-icon'/>
+          
         </button>
       </div>
     </header>
